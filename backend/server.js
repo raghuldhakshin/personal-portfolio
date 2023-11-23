6147d6e920a4960a2a67171c09670415d100e5dd
raghuldhakshin <70769734+raghuldhakshin@users.noreply.github.com>
2023-11-23 12:49:31 +0530

    Update server.js
    Port number change backend

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/", router);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT_BACKEND || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
